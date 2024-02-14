0bfc9bc520dc8cf1c74579e307ea357c572c121e
stephanie-115 <stephanie.terese@gmail.com>
2024-02-14 10:02:47 -0500

    About section

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var navbar = document.querySelector(".navbar");
+  
+    window.addEventListener("scroll", function () {
+      if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+      } else {
+        navbar.classList.remove("scrolled");
+      }
+    });
+  });
+  
